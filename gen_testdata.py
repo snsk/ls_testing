442098e121045ef634a6274d8c9ea33ce1063921
snsk <snsk@users.noreply.github.com>
2022-05-04 12:03:07 +0900

    Update gen_testdata.py

--- gen_testdata.py
+++ gen_testdata.py
@@ -1,7 +1,14 @@
 import os
 
+# Functional::Spec Confirmance::Basic Behavior
+
+new_dir_path = 'Functional_SpecConfirmance_BasicBehavior'
+
+if not os.path.exists(new_dir_path):
+    os.makedirs(new_dir_path)
+
 file_list = ['aaa', 'bbb', 'ccc']
 
 for file_name in file_list:
-    f = open(file_name, 'w')
+    f = open('./Functional_SpecConfirmance_BasicBehavior/'+file_name, 'w')
     f.close()
