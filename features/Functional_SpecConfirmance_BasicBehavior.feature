5f59d123f7844ae5af114737fd8d8ae98b6d4746
snsk <snsk@users.noreply.github.com>
2022-05-21 15:45:07 +0900

    impl test case

--- features/Functional_SpecConfirmance_BasicBehavior.feature
+++ features/Functional_SpecConfirmance_BasicBehavior.feature
@@ -12,3 +12,23 @@ Feature: Functional_SpecConfirmance_BasicBehavior
     Given current directory.
     When the ls command is invoked with Terminal.
     Then the output is in columns (sorted vertically).
+
+  Scenario: test case id 3
+    Given current directory.
+    When the ls command is invoked with Terminal.
+    Then control characters are output as question marks    
+
+  Scenario: test case id 4
+    Given current directory.
+    When the ls command is invoked with non-option.
+    Then the output is listed one per line
+
+  Scenario: test case id 5
+    When the ls command is invoked with non-option.
+    Then control characters are output as-is
+
+  Scenario: test case id 6
+    Given current directory.
+    When the ls command is invoked with non-option.
+    Then lists the contents of directories
+  
--- features/steps/Functional_SpecConfirmance_BasicBehavior.py
+++ features/steps/Functional_SpecConfirmance_BasicBehavior.py
@@ -9,11 +9,16 @@ new_dir_path = 'Functional_SpecConfirmance_BasicBehavior'
 if not os.path.exists(new_dir_path):
     os.makedirs(new_dir_path)
 
-file_list = ['aaa', 'bbb', 'ccc']
+file_list = ['aaa', 'bbb', 'ccc', 'd\ad']
 
 for file_name in file_list:
     f = open('./'+new_dir_path+'/'+file_name, 'w')
     f.close()
+
+if not os.path.exists('./'+new_dir_path+'/eee/'):
+    os.makedirs('./'+new_dir_path+'/eee/')
+
+expected_normal_output = b'aaa\nbbb\nccc\nd\x07d\neee\n'
 
 @given(u'current directory.')
 def step_impl(context):
@@ -25,17 +30,32 @@ def step_impl(context):
 @when(u'the ls command is invoked with non-option.')
 def step_impl(context):
     context.res_no_arg = subprocess.run("ls", capture_output=True)
-    context.res_dot  = subprocess.run(["ls", "."], capture_output=True)
+    print(context.res_no_arg.stdout)
 
 @then(u'same as invoked with a single argument of ‘.’.')
 def step_impl(context):
+    context.res_dot  = subprocess.run(["ls", "."], capture_output=True)
     assert_that(context.res_no_arg.stdout, equal_to(context.res_dot.stdout))
 
-@when(u'the ls command is invoked with Terminal.')#TODO: make not clear the "invoked Terminal"
+@when(u'the ls command is invoked with Terminal.') #TODO: make not clear the "invoked Terminal"
 def step_impl(context):
     context.hyphen1  = subprocess.run(["ls", "-1"], capture_output=True)
 
 @then(u'the output is in columns (sorted vertically).')
+@then(u'control characters are output as question marks') #TODO: make not clear the "invoked Terminal"
 def step_impl(context):
-    print(context.hyphen1.stdout)
-    assert_that(context.hyphen1.stdout, equal_to(b'aaa\nbbb\nccc\n'))
+    assert_that(context.hyphen1.stdout, equal_to(expected_normal_output))
+
+@then(u'the output is listed one per line')
+@then(u'control characters are output as-is')
+@then(u'lists the contents of directories')
+def step_impl(context):
+    assert_that(context.res_no_arg.stdout, equal_to(expected_normal_output))
+
+
+
+
+
+
+
+
