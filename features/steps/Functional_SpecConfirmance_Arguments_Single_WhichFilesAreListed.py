9f5b51005058922ee0605cd2dba36af6edbfb5d5
snsk <snsk@users.noreply.github.com>
2022-06-17 15:42:20 +0900

    Update Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed.py
    add test environment size, width

--- features/steps/Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed.py
+++ features/steps/Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed.py
@@ -9,7 +9,7 @@ new_dir_path = 'Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed'
 if not os.path.exists(new_dir_path):
     os.makedirs(new_dir_path)
 
-file_list = ['aaa', 'bbb', 'ccc', 'ddd~', '.eee']
+file_list = ['aaa', 'bbbb', 'ccccc', 'dddddd~', '.eeeeeee']
 
 for file_name in file_list:
     f = open('./'+new_dir_path+'/'+file_name, 'w')
@@ -20,5 +20,11 @@ if not os.path.exists('./'+new_dir_path+'/fff/'):
 
 if not os.path.exists('./'+new_dir_path+'/ggg_sl'):
     os.symlink('aaa', './'+new_dir_path+'/ggg_sl')
+
+if not os.path.exists('./'+new_dir_path+'/hhh_16'):
+    subprocess.run(["fallocate", "-l", "16", "./"+new_dir_path+"/"+"hhh_16"], capture_output=True)
+
+if not os.path.exists('./'+new_dir_path+'/hhh_32'):
+    subprocess.run(["fallocate", "-l", "32", "./"+new_dir_path+"/"+"hhh_32"], capture_output=True)
 
 expected_normal_output = b'aaa\nbbb\nccc\nd\x07d\neee\n'
