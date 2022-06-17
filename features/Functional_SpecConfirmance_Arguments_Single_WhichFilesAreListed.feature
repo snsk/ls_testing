3640261cdf4bb89cb9e3bf0ad30cce5ee48652f4
snsk <snsk@users.noreply.github.com>
2022-06-17 16:11:04 +0900

    add feature file and add ID1 sample

---
+++ features/Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed.feature
@@ -0,0 +1,9 @@
+Feature: Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed
+
+# test case id has refer from:
+#https://gihoz.com/users/snsk/repositories/ls_testing/documents/cause_flow_diagram/ffb93c29-1194-4d49-bf7c-aa66abc107d4
+
+  Scenario: test case id 1
+    Given with Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed directory.
+    When the ls command is invoked with -a option.
+    Then do not ignore file names that start with dot.
--- features/steps/Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed.py
+++ features/steps/Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed.py
@@ -9,7 +9,7 @@ new_dir_path = 'Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed'
 if not os.path.exists(new_dir_path):
     os.makedirs(new_dir_path)
 
-file_list = ['aaa', 'bbbb', 'ccccc', 'dddddd~', '.eeeeeee']
+file_list = ['aaa.a', 'bbbb.a', 'ccccc.b', 'dddddd.b~', '.eeeeeee.b']
 
 for file_name in file_list:
     f = open('./'+new_dir_path+'/'+file_name, 'w')
@@ -19,7 +19,7 @@ if not os.path.exists('./'+new_dir_path+'/fff/'):
     os.makedirs('./'+new_dir_path+'/fff/')
 
 if not os.path.exists('./'+new_dir_path+'/ggg_sl'):
-    os.symlink('aaa', './'+new_dir_path+'/ggg_sl')
+    os.symlink('aaa.a', './'+new_dir_path+'/ggg_sl')
 
 if not os.path.exists('./'+new_dir_path+'/hhh_16'):
     subprocess.run(["fallocate", "-l", "16", "./"+new_dir_path+"/"+"hhh_16"], capture_output=True)
@@ -27,4 +27,16 @@ if not os.path.exists('./'+new_dir_path+'/hhh_16'):
 if not os.path.exists('./'+new_dir_path+'/hhh_32'):
     subprocess.run(["fallocate", "-l", "32", "./"+new_dir_path+"/"+"hhh_32"], capture_output=True)
 
-expected_normal_output = b'aaa\nbbb\nccc\nd\x07d\neee\n'
+expected_normal_output = b'aaa.a\nbbbb.a\nccccc.b\ndddddd.b~\nfff\nggg_sl\nhhh_16\nhhh_32\n'
+
+@given(u'with Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed directory.')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given with Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed directory.')
+
+@when(u'the ls command is invoked with -a option.')      
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the ls command is invoked with -a option.')
+
+@then(u'do not ignore file names that start with dot.')  
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then do not ignore file names that start with dot.')
