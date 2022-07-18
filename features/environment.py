6b574148abf93689aa3d4574271f08694ab4829e
snsk <snsk@users.noreply.github.com>
2022-07-18 12:26:49 +0900

    Update environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,7 @@
 from behave import fixture, use_fixture
 import os
 
+# It is necessary to chage directory after one feature is done 
+# due to working directory be previus testing envioroment.
 def after_feature(context, feature):
     os.chdir('../')
