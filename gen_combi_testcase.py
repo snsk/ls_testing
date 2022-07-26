3fbd9ac7bd6c6417eef10f79057b916079c82e52
snsk <snsk@users.noreply.github.com>
2022-07-26 14:54:34 +0900

    prepare auto test for combination

--- gen_combi_testcase.py
+++ gen_combi_testcase.py
@@ -1,21 +1,25 @@
 import os
 import subprocess, sys
 import itertools
+import combi_testware
 
 # The file is mainly tested to confirm the expected results.
 
-args = ('-a', '-A', '-B', '-d', '-H', '--sort=size', '--sort=width', '--sort=extension', '--sort=none', '--hide=`*.a', '--ignore=*.a', '-L', '-R')
-arg_combi_list = list(itertools.combinations(args ,2))
-print(arg_combi_list)
+print(len(combi_testware.combi_testcase_list))
+print(len(combi_testware.combi_expected_result))
 
 exit()
 
+args = ('-a', '-A', '-B', '-d', '-H', '--sort=size', '--sort=width', '--sort=extension', '--sort=none', '--hide=`*.a', '--ignore=*.a', '-L', '-R')
+arg_combi_list = list(itertools.combinations(args ,2))
+
+print('[')
 for i, item in enumerate(arg_combi_list):
-    print(str(i)+':'+ item[0]+', '+item[1])
     ret = subprocess.run([
         "ls", 
         "Functional_SpecConfirmance_Arguments_Single_WhichFilesAreListed",
         item[0],
         item[1],
         ], capture_output=True)
-    print(ret.stdout)
+    print('('+str(ret.stdout)+'),')
+print(']')
