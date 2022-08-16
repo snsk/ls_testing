5c0847f9d7f217a0bd9e1395e648f062999ba0b2
snsk <snsk@users.noreply.github.com>
2022-08-16 13:10:00 +0900

    hierarchy fuzzing run native 100 times.

--- HierarchyFuzzing/hierarchy_fuzzing.py
+++ HierarchyFuzzing/hierarchy_fuzzing.py
@@ -1,25 +1,31 @@
 import string
 import math
 import random
-import randomfiletree
+import subprocess
+import os
 
 class TestEnvironmentGenerator():
 
-    @staticmethod
-    def gen_random_file_name():
-        char_array = list(string.printable)
-        char_array.remove('/')
+    __char_array = list(string.printable)
+    __char_array.remove('/')
+
+    @classmethod
+    def gen_random_file_name(cls):    
         filename_length_seed = math.floor(random.random()*255)
         filename = ''
         for _ in range(filename_length_seed):
-            filename = filename + random.choice(char_array)
+            filename = filename + random.choice(cls.__char_array)
         return filename
-
-    def setup():
-        pass
-
 
 class HierarchyFuzzing():
 
-    def run_ls_command(args, seed):
-        pass
+    def run_ls_command(workdir):
+        os.chdir(workdir)
+        ret = subprocess.run([
+            "ls", "-a", "-R", "-l" 
+            ], capture_output=True)
+        print(ret.returncode)
+        #print(ret.stdout)
+        #print(ret.stderr)
+
+
--- HierarchyFuzzing/main.py
+++ HierarchyFuzzing/main.py
@@ -1,3 +1,24 @@
+import randomfiletree
 import hierarchy_fuzzing as hf
+import random
+import string
+import shutil
+import os
+import time
 
-print(hf.TestEnvironmentGenerator.gen_random_file_name())
+workdir = './tmp'
+round = 100
+print('workdir:' + os.getcwd())
+
+for i in range(round):
+
+    randomfiletree.core.iterative_gaussian_tree(
+        workdir,
+        nfiles=100,
+        nfolders=10,
+        maxdepth=100,
+        filename = hf.TestEnvironmentGenerator.gen_random_file_name
+    )
+    hf.HierarchyFuzzing.run_ls_command(workdir)
+    os.chdir('..')
+    shutil.rmtree(workdir)
